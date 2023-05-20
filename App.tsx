b29481a80f9f30cc8414fee4cbbbf9143436bbc5
maaz <marzusness@gmail.com>
2023-05-20 21:12:51 +0300

    Finish

--- App.tsx
+++ App.tsx
@@ -1,27 +1,5 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import Front from "./src/views/Front";
 
 export default () => {
-  return (
-    <View style={s.container}>
-      <StatusBar style="auto" />
-
-      <Text style={s.welcomeText}>Happy coding!</Text>
-    </View>
-  );
+  return <Front />;
 };
-
-const s = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  welcomeText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
